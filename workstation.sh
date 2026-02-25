c96b4e3cc4bf065504c7d16895ca35a38b804473
madhankotte <madhankotte20@email.com>
2026-02-25 15:07:39 +0530

    Initial commit with eks.yaml and workstation.sh

---
+++ workstation.sh
@@ -0,0 +1,20 @@
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo 
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+
+
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.0/2026-01-29/bin/darwin/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
+tar -xzf eksctl_Linux_amd64.tar.gz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
